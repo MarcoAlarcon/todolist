047ffe1fe58727fd3f8a21893180e665a80ba16b
Marco Alarcon <85081442+MarcoAlarcon@users.noreply.github.com>
2023-06-28 23:36:11 -0300

    Novo Widget de lista e funcionalidades.
    Criado novo widget Tasklist que vai conter as tarefas que forem criadas. Criado funcionalidade de resetar o campo ao digitar a tarefa.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +11,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Just tasks',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 34, 33, 33)),
+    return ChangeNotifierProvider(
+      create: (_) => TaskListProvider(),
+      child: MaterialApp(
+        title: 'Just tasks',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 34, 33, 33)),
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
+  }
+}
+
+class TaskListProvider extends ChangeNotifier {
+  List<String> taskList = [
+    'Task 1',
+    'task2',
+    'task3',
+    'task4',
+    'task6',
+    'task7',
+    'task8',
+    'task9',
+    'task10'
+  ];
+}
+
+class TaskList extends StatefulWidget {
+  const TaskList({Key? key}) : super(key: key);
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskListProvider>(
+        builder: (context, taskListProvider, child) {
+      return Material(
+        child: ListView.builder(
+          itemCount: taskListProvider.taskList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(taskListProvider.taskList[index]),
+            );
+          },
+        ),
+      );
+    });
   }
 }
 
@@ -86,49 +130,60 @@ class DashList extends StatelessWidget {
       child: const Padding(
         padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
         child: Column(
-          children: [CreateTask()],
+          children: [
+            CreateTask(),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 320,
+              height: 400,
+              child: TaskList(),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class CreateTask extends StatelessWidget {
-  const CreateTask({super.key});
+class CreateTask extends StatefulWidget {
+  const CreateTask({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-
-    return Card(
-      child: TextField(
-        obscureText: false,
-        cursorColor: const Color.fromARGB(255, 223, 223, 223),
-        style: const TextStyle(color: Color.fromARGB(255, 223, 223, 223)),
-        decoration: InputDecoration(
-            labelStyle:
-                const TextStyle(color: Color.fromARGB(255, 223, 223, 223)),
-            filled: true,
-            fillColor: const Color.fromARGB(255, 19, 18, 18),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            focusedBorder: const OutlineInputBorder(
-                borderSide:
-                    BorderSide(color: Color.fromARGB(255, 223, 223, 223))),
-            labelText: 'New Task'),
-        onSubmitted: (task) {
-          print(task);
-        },
-      ),
-    );
-  }
+  State<CreateTask> createState() => _CreateTaskState();
 }
 
-class TaskList extends StatelessWidget {
-  const TaskList({super.key});
-
+class _CreateTaskState extends State<CreateTask> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Text('teste');
+    return Card(
+      child: SizedBox(
+        height: 60,
+        child: TextField(
+          controller: _textController,
+          obscureText: false,
+          cursorColor: const Color.fromARGB(255, 223, 223, 223),
+          style: const TextStyle(color: Color.fromARGB(255, 223, 223, 223)),
+          decoration: InputDecoration(
+              labelStyle:
+                  const TextStyle(color: Color.fromARGB(255, 223, 223, 223)),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 19, 18, 18),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 223, 223, 223))),
+              labelText: 'New Task'),
+          onSubmitted: (task) {
+            print(task);
+            _textController.clear();
+          },
+        ),
+      ),
+    );
   }
 }
